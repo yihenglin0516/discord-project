c9967a476d4ca5ce8e469664b527b2bed84eccc8
yihenglin0516 <b08901161@ntu.edu.tw>
2021-06-30 11:35:02 +0800

    fix: 處理node格式問題 問題:統一回傳值格式 原因:方便主程式端作業 調整項目:更改node內容，統一funciton名稱

--- hash.py
+++ hash.py
@@ -4,7 +4,7 @@ class Hash:
     def __init__(self,size):
         self.size=size
         self.table=[linked_list.Linked_list() for i in range(size)]
-    def _insert(self,item):  #item是list
+    def insert(self,item):  #item是list
         index=0
         key=item[0]+' '+item[1]
         for i,character  in enumerate(key): #hashing 
@@ -13,13 +13,13 @@ class Hash:
         print('key= '+key+'index= '+str(index))
         _item=linked_list.node(item)
         self.table[index].push(_item)
-    def output(self): #只show出基本的info 
+    def list_all(self): #只show出基本的info 
         result=''
         for i in self.table:
             if i.root:
                 result+=i.show()
         return result 
-    def find(self,key):
+    def search(self,key):
         index=0
         key=str.strip(key)
         print(key)
@@ -30,7 +30,7 @@ class Hash:
         target=self.table[index]
         pointer=target.root
         while pointer:
-            _key=pointer.value[0]+' '+pointer.value[1]
+            _key=pointer.Name+' '+pointer.Teacher
             if _key==key:
                 return pointer
             else:
