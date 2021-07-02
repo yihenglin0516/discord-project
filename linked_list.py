af8a6d2427480a8b91605a92449a33e503b21bd6
yihenglin0516 <b08901161@ntu.edu.tw>
2021-07-02 16:35:39 +0800

    fix:因應新的attribute更改程式碼

--- linked_list.py
+++ linked_list.py
@@ -3,7 +3,8 @@ class node:
         self.Name = data[0]
         self.Teacher = data[1]
         self.Time = data[2]
-        self.NTUcourse = data[3]
+        self.NTUcourse = data[4]
+        self.credit=data[3]
         self.comment=''
         self.great_points = 0
         self.disliked_points = 0
@@ -11,7 +12,7 @@ class node:
         self.next=None
         
     def output(self):
-        result=self.Name+' '+self.Teacher+' '+self.Time+'讚'+str(self.great_points)+' '+'倒讚'+str(self.disliked_points)+'\n'+self.NTUcourse+'\n'
+        result="課程名稱:"+self.Name+" "+"\n"+"教師姓名:"+self.Teacher+"\n"+"上課時間:"+self.Time+"\n"+"學分:"+self.credit+"\n"+"ntu course: "+self.NTUcourse
         if self.comment!='' :
             result+=self.comment+'\n'
 
