c15e889700f15cf09671fb7dbeb28f55a0df4f3f
yihenglin0516 <b08901161@ntu.edu.tw>
2021-07-02 11:09:00 +0800

    fix:處理重複輸入會造成table裡有重複結果 fix:show_all中當檔案是空的現在會回傳訊息 feat:增加存檔功能

--- bot.py
+++ bot.py
@@ -7,20 +7,41 @@ from add_table import add_to_table
 from  show_table import show_table
 from delete_from_table import  delete_from_table
 bot=commands.Bot(command_prefix="!")
+## initial 
 table=hash.Hash(150)
+
+with open('data.txt','r',encoding="utf-8") as data:
+    for line in data :
+        line=line.split('!')
+        line[3]=line[3][:-1]
+        table.insert(line)
+    
 
 @bot.command()
 async def show(ctx,arg):
     
     name,teacher,when=show_info(arg)
-    link=search_class(name,teacher)
+    if when:   #檢查傳回值是否異常
+        when=when.strip()
+    if not when:
+        when=''
+    link="https://www.ptt.cc/bbs/NTUcourse/search?q=+"+teacher
     message="課程名稱:"+name+" "+"\n"+"教師姓名:"+teacher+"\n"+"上課時間"+when+"\n"+"ntu course: "+link
     await ctx.send(message)
+    with open('data.txt','a',encoding="utf-8") as data:
+        data.write(name+'!'+teacher+'!'+when+'!'+link+'\n')
     _list=[name,teacher,when,link]
-    table.insert(_list)
+    key=name+' '+teacher       #新增key尋找有沒有已經被加入過了
+
+    if type(table.search(key))==str:
+        table.insert(_list)
+    else:
+        pass
 @bot.command()    
 async def show_all(ctx):
     result=table.list_all()
+    if result=='':
+        result='no class inside'
     await ctx.send(result)
 @bot.command()   
 async def find(ctx,arg1,arg2):
@@ -31,6 +52,7 @@ async def find(ctx,arg1,arg2):
         await ctx.send(pointer)
     else:
         result=pointer.output()
+        
         await ctx.send(result)
 
 @bot.command()
@@ -61,5 +83,4 @@ async def liked(ctx,arg1,arg2):
 async def disliked(ctx,arg1,arg2):
     arg = arg1+' '+arg2
     table.disliked_points(arg)
-
 
