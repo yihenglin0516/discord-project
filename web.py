74491f756d8c1dcd9819c43e73d0db43ddb021d4
yihenglin0516 <b08901161@ntu.edu.tw>
2021-07-02 16:33:42 +0800

    feat:增加回傳credit的功能 fix:在教師姓名為none時可以正確運作

--- web.py
+++ web.py
@@ -5,7 +5,11 @@ def show_info(url):
     html.encoding='utf_8'
     soup = BeautifulSoup(html.content, 'html.parser')
     teacher=soup.find_all('a')[0].string
+    if not teacher:
+        teacher=''
     name=soup.find_all('td')[3].contents[0]
     when=soup.find_all('td')[23].string
-    return name,teacher,when
+    credit= soup.find_all('td')[17].string
+    return name,teacher,when,credit
+
     
