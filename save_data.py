be57a1b483fe90b19d5e6c498c21bdaa47166514
yihenglin0516 <b08901161@ntu.edu.tw>
2021-07-03 17:52:39 +0800

    feat:把資料放上雲端 問題:原本的寫法只有開啟主程式的電腦才能存取資料 修改項目:把資料傳到jsonstorage上

--- save_data.py
+++ save_data.py
@@ -1,32 +1,31 @@
 from hash import Hash
 import json 
+import requests
 ##在檔案裡面沒東西的時候會有問題
 class Data (): #裡面放不同的hash
     def __init__(self):
         self.database={} 
     ##存檔存不進去
     def save(self,ctx): #ctx為context物件
-        try:   
-            f=open('data.json','r',encoding='utf-8')
-            raw=json.load(f) #return a dictionary with key=server name and value=list of class
-        except :
-            raw={}
+        
+        response=requests.get("https://api.jsonstorage.net/v1/json/f8301287-69bc-4166-90e7-7e51f02dd224")   
+        raw=response.json()
         #寫入
         node_list=self.database[ctx].get_all_node()
         raw[ctx]=[] #清空再重新寫入
         for node in node_list:
             raw[ctx].append([node.Name,node.Teacher,node.Time,node.credit,node.great_points,node.disliked_points,node.comment])
         print(raw)
-        f.close()
-        f=open('data.json','w',encoding='utf-8')
-        json.dump(raw,f)
-        f.close()
+        update=requests.put("https://api.jsonstorage.net/v1/json/f8301287-69bc-4166-90e7-7e51f02dd224" 
+        ,json=raw
+        )
+        print(update)
     
     def load(self,ctx):
         self.database[ctx]=Hash(150)
         try:
-            f=open('data.json','r',encoding='utf-8')
-            raw=json.load(f)
+            response=requests.get("https://api.jsonstorage.net/v1/json/f8301287-69bc-4166-90e7-7e51f02dd224")   
+            raw=response.json()
             data=raw[ctx]  #回傳裝有課程資料的list
         except KeyError: #新的channel
             raw={ctx:[]}
@@ -35,4 +34,4 @@ class Data (): #裡面放不同的hash
         
         for c in data :
             self.database[ctx].insert(c)
-        f.close()    
+       
