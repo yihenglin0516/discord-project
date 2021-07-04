c0d22e576fe3be6f6a77296f551088d3d64f3363
yihenglin0516 <b08901161@ntu.edu.tw>
2021-07-04 11:05:57 +0800

    fix:when回傳值不一 問題:when在沒有值時回傳值不一 調整項目:在web回傳時就將type轉為str

--- web.py
+++ web.py
@@ -4,12 +4,12 @@ def show_info(url):
     html=requests.get(url)
     html.encoding='utf_8'
     soup = BeautifulSoup(html.content, 'html.parser')
-    teacher=soup.find_all('a')[0].string
+    teacher=str(soup.find_all('a')[0].string).strip()
     if not teacher:
         teacher=''
-    name=soup.find_all('td')[3].contents[0]
-    when=soup.find_all('td')[23].string
-    credit= soup.find_all('td')[17].string.strip()
+    name=str(soup.find_all('td')[3].contents[0]).strip()
+    when=str(soup.find_all('td')[23].string).strip()
+    credit= str(soup.find_all('td')[17].string).strip()
     return name,teacher,when,credit
 
     
